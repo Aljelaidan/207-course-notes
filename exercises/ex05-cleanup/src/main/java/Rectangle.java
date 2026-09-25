0b28467b4d1c22ce909c1bb47bf5d4e7ca113c80
Mohammed Aljelaidan <aljelaidan2006@gmail.com>
2026-09-25 06:41:40 +0300

    Finished TODO

--- exercises/ex05-cleanup/src/main/java/Rectangle.java
+++ exercises/ex05-cleanup/src/main/java/Rectangle.java
@@ -1,29 +1,51 @@
+/**
+ * Represents a 2D rectangle with a given width and height.
+ */
 public class Rectangle {
-    private double width;
-    private double height;
+  private double width;
+  private double height;
 
-    public Rectangle(double w,double h){
-        this.width=w;
-        this.height=h;
-    }
+  /**
+   * Constructs a new Rectangle with the specified width and height.
+   *
+   * @param w the initial width of the Rectangle
+   * @param h the initial height of the Rectangle
+   */
+  public Rectangle(double w, double h) {
+    this.width = w;
+    this.height = h;
+  }
 
-    public double area(){
-        return width*height;
-    }
+  /**
+   * Calculates and returns the area of the Rectangle.
+   *
+   * @return the area of the Rectangle
+   */
+  public double area() {
+    return width * height;
+  }
 
-    /**
-     * scales the rectangle
-     * @param factor
-     */
-    public void scale(double factor) {
-      width = width * factor;
-      height = height * factor;
-    }
+  /**
+   * scales the rectangle.
+   *
+   * @param factor the scaling factor to apply to width and height
+   */
+  public void scale(double factor) {
+    width = width * factor;
+    height = height * factor;
+  }
 
-    public boolean isLargerThan(Rectangle other){
-        if(area() > other.area())
-            return true;
-        else
-            return false;
+  /**
+   * compares the area of this Rectangle to another Rectangle.
+   *
+   * @param other the other Rectangle to compare with
+   * @return true if this Rectangle has a larger area than the other, false otherwise
+   */
+  public boolean isLargerThan(Rectangle other) {
+    if (area() > other.area()) {
+      return true;
+    } else {
+      return false;
     }
+  }
 }
